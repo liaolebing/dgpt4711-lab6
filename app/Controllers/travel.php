a80bb2f739d20e9cd0eb30c6cdf352c6781404dd
liaolebing <1744098752@qq.com>
2020-11-12 20:38:45 +0800

    lab8

--- app/Controllers/travel.php
+++ app/Controllers/travel.php
@@ -12,9 +12,34 @@ class Travel extends BaseController
         // get a template parser
     $parser = \Config\Services::parser();
         // tell it about the substitions
-    return $parser->setData(['records' => $records])
-        // and have it render the template with those
-    ->render('placeslist');
+    $table = new \CodeIgniter\View\Table();
+    
+    $headings = $places->fields;
+    $displayHeadings = array_slice($headings, 1, 2); 
+    $table->setHeading(array_map('ucfirst', $displayHeadings));
+    
+    foreach ($records as $record) 
+    {
+        $nameLink = anchor("travel/showme/$record->id",$record->name);
+        $table->addRow($nameLink,$record->description);
+    }
+    
+    $template = [
+                'table_open' => '<table cellpadding="5px">',
+                'cell_start' => '<td style="border: 1px solid #dddddd;">',
+                'row_alt_start' => '<tr style="background-color:#dddddd">',
+                ];
+    $table->setTemplate($template);
+    
+   $fields = [
+       'title' => 'Travel Destinations',
+       'heading' => 'Travel Destinations',
+       'footer' => 'lyx'];
+  
+    return $parser->setData($fields)->render('templates\top') .
+           $table->generate().
+           $parser->setData($fields)->render('templates\bottom');
+    
     }
     public function showme($id)
     {
@@ -25,8 +50,31 @@ class Travel extends BaseController
       // get a template parser
       $parser = \Config\Services::parser();
       // tell it about the substitions
-      return $parser->setData($record)
-      // and have it render the template with those
-      ->render('oneplace');
+      
+       $table = new \CodeIgniter\View\Table();
+    
+    $headings = $places->fields;
+    $table->addRow($headings[0],$record['id'])
+          ->addRow($headings[1],$record['name'])
+          ->addRow($headings[2],$record['description'])
+          ->addRow($headings[3],$record['link'])
+          ->addRow($headings[4],"<img src=\"/image/".$record['image']."\"/>");
+
+    $template = [
+                'table_open' => '<table cellpadding="5px">',
+                'cell_start' => '<td style="border: 1px solid #dddddd;">',
+                'row_alt_start' => '<tr style="background-color:#dddddd">',
+                ];
+    $table->setTemplate($template);
+    
+   $fields = [
+       'title' => 'Travel Destinations',
+       'heading' => 'Travel Destinations',
+       'footer' => 'yingxianLiao'
+       ];
+      
+   return $parser->setData($fields)->render('templates\top') .
+           $table->generate().
+           $parser->setData($fields)->render('templates\bottom');
     }
 }
